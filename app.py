606b0aa82cb06c2a0e9fd4f37f1e76c111c07ea4
NathanSachombe <nathanguyo6@gmail.com>
2024-10-22 12:28:30 +0300

    Python variables and data types

---
+++ app.py
@@ -0,0 +1,32 @@
+# Test
+
+print("Hello world!")
+
+# 1.Draw a shape
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# 2. Variables and Data Types
+
+character_name = "John" # String
+
+print("There was a man named " + character_name + ".")
+
+character_name = "Mike"
+character_age = 50 # Int
+is_male = True # Boolean
+print("There was a man named " + character_name + ".")
+print("He was " + character_age + " years old.")
+
+# 3. Working with Strings
+
+print("Giraffe Academy")
+print("Giraffe\nAcademy") # New line
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+
+
+
