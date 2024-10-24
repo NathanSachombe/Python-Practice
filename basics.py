f34cc8db437a780d31447e0f5c3a02c885732c82
NathanSachombe <nathanguyo6@gmail.com>
2024-10-24 12:47:39 +0300

    Basics.py

---
+++ basics.py
@@ -0,0 +1,78 @@
+# Test
+
+print("Hello world!")
+
+# 1.Draw a shape
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+# 2. Variables and Data Types
+
+character_name = "John" # String
+
+print("There was a man named " + character_name + ".")
+
+character_name = "Mike"
+character_age = 50 # Int
+is_male = True # Boolean
+print("There was a man named " + character_name + ".")
+
+
+# 3. Working with Strings
+
+print("Giraffe Academy")
+print("Giraffe\nAcademy") # New line
+phrase = "Giraffe Academy"
+print(phrase + " is cool!")
+
+# 3.1 String functions
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[0])
+print(phrase[3])
+print(phrase.index("G"))
+print(phrase.index("a"))
+print(phrase.index("Acad"))
+print(phrase.replace("Giraffe", "Elephant"))
+
+# 4. Working with Numbers
+
+num = 5
+
+print(2)
+print(2.43)
+print(-5.76)
+print(4 * 5) # Multiplication
+print(9 / 3) # Division
+print(3 * (4 * 5)) # Order of operations
+
+print(str(num)) # Convert number to string
+print(abs(num)) # Absolute value
+print(pow(num, 2)) # Power to
+print(max(4, 6)) # Max number
+print(min(4, 6)) # Min number
+print(round(4.8)) # Round off number
+
+from math import *   # Import math module to get more maths functions
+
+print(floor(3.7)) # Round to the smallest number
+print(ceil(3.7)) # Round to the biggest number
+print(sqrt(25)) # Square root
+
+
+
+
+
+
+
+
+
+
+
+
