b761c9a982a7807ec3a87ed9017459e7e6429f15
NathanSachombe <nathanguyo6@gmail.com>
2024-11-04 15:14:46 +0300

    While loop

---
+++ while loop.py
@@ -0,0 +1,10 @@
+# Do something while a certain condition is met
+# Loop through code until a certain condition becomes false
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1     # Increment i
+
+print("Done with loop")
+
