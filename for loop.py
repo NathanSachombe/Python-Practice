67d6a306b99d390dbbe4243821625f1cdb7e6670
NathanSachombe <nathanguyo6@gmail.com>
2024-11-04 15:51:14 +0300

    Exponent function

--- for loop.py
+++ for loop.py
@@ -19,3 +19,15 @@ for index in range(5):
     else:
         print("Not first")
 
+
+# Exponent function using for loop(Raise to power)
+def raise_to_power(base_num, pow_num):
+    result = 1
+    for index in range(pow_num):
+        result = result * base_num
+    return result
+
+print(raise_to_power(3, 4))
+
+
+
