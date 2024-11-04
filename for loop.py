27b89e544c270ac928efcf0b821edd211eab638e
NathanSachombe <nathanguyo6@gmail.com>
2024-11-04 15:44:16 +0300

    For Loop

---
+++ for loop.py
@@ -0,0 +1,21 @@
+# Iterate through something
+
+for letter in "Giraffe Academy":        # Loop through a string
+    print(letter)
+
+friends = ["Sandra", "Karen", "Zoe"]     # Loop through an array
+for friend in friends:
+    print(friend)
+
+for index in range(10): # all numbers from 0 to 10 but not including 10
+    print(index)
+
+for index in range(2, 5): # numbers between 2 and 5 but not including 5
+    print(index)
+
+for index in range(5):
+    if index == 0:
+        print("First iteration")
+    else:
+        print("Not first")
+
